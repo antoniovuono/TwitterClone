a209167977002535b6a4b7a46e865964e829bf23
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-18 18:38:26 -0300

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Antonio Vuono on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
