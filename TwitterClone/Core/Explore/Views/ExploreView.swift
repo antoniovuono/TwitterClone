1f2c069a3ad78e5a350ddf4bc81b8593c3fdbaf9
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-19 21:45:18 -0300

    Explore screen, user component and goback button from profile created.

---
+++ TwitterClone/Core/Explore/Views/ExploreView.swift
@@ -0,0 +1,37 @@
+//
+//  ExploreView.swift
+//  TwitterClone
+//
+//  Created by Antonio Vuono on 18/05/22.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVStack {
+                        ForEach(0 ... 25, id: \.self) { _ in
+                            NavigationLink {
+                                ProfileView()
+                            } label: {
+                                UserRowView()
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
