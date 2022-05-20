2f7ce2cb4ef117856ea4163059c8972a9a326fe7
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-20 09:22:16 -0300

    Layout from side menu created

--- TwitterClone/Core/SideMenu/ViewModel/SideMenuViewModel.swift
+++ TwitterClone/Core/SideMenu/ViewModel/SideMenuViewModel.swift
@@ -14,7 +14,7 @@ enum SideMenuViewModel: Int, CaseIterable {
     case bookmarks
     case logout
     
-    var description: String {
+    var title: String {
         switch self {
         case .profile: return "Profile"
         case .lists: return "Lists"
--- TwitterClone/Core/SideMenu/Views/SideMenuView.swift
+++ TwitterClone/Core/SideMenu/Views/SideMenuView.swift
@@ -10,21 +10,42 @@ import SwiftUI
 struct SideMenuView: View {
     var body: some View {
         
-        VStack(alignment: .leading) {
-            Circle()
-                .frame(width: 48, height: 48)
-        
-            VStack(alignment: .leading, spacing: 4) {
-                Text("Antonio Vuono")
-                    .font(.headline)
-                
-                Text("@asvuono")
-                    .font(.caption)
-                    .foregroundColor(.gray)
-            }
+        VStack(alignment: .leading, spacing: 32) {
+            VStack(alignment: .leading) {
+                Circle()
+                    .frame(width: 48, height: 48)
             
-            UserStatsView()
-                .padding(.vertical)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Antonio Vuono")
+                        .font(.headline)
+                    
+                    Text("@asvuono")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                
+                UserStatsView()
+                    .padding(.vertical)
+            }
+            .padding(.leading)
+            
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { option in
+                HStack(spacing: 16) {
+                    Image(systemName: option.imageName)
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    
+                    Text(option.title)
+                        .font(.subheadline)
+                    
+                    Spacer()
+                }
+                .frame(height: 40)
+                .padding(.horizontal)
+            }
+        
+            
+            Spacer()
         }
     }
 }
