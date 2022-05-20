4a0fb02ade72c25793d57744cbdc86c2f86d8ff2
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-20 15:19:11 -0300

    Making sidemenu work

--- TwitterClone/ContentView.swift
+++ TwitterClone/ContentView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showMenu = false
+    
     var body: some View {
         ZStack(alignment: .topLeading) {
             MainTabView()
+                .navigationBarHidden(showMenu)
             
+            if showMenu {
+                ZStack {
+                    Color(.black)
+                    .opacity(0.25)
+                }.onTapGesture {
+                    withAnimation(.easeInOut) {
+                        showMenu = false
+                    }
+                }
+            }
+            
+            SideMenuView()
+                .frame(width: 300)
+                .offset(x: showMenu ? 0: -300, y: 0)
+                .background(showMenu ? Color.white : Color.clear)
+        }
+        .navigationTitle("Home")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    showMenu.toggle()
+                } label: {
+                    Circle()
+                    .frame(width: 32, height: 32)
+                }
+            }
         }
     }
     
@@ -19,6 +50,8 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
